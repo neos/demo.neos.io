1a975094eda41d6d307784513aa27f3cd7cde377
Stefan Regniet <s.regniet@techdivision.com>
2022-07-20 11:54:33 +0200

    TASK: Add matomo tracking

--- DistributionPackages/Neos.Demo.UserRegistration/Classes/Package.php
+++ DistributionPackages/Neos.Demo.UserRegistration/Classes/Package.php
@@ -2,8 +2,10 @@
 namespace Neos\Demo\UserRegistration;
 
 use Neos\Demo\UserRegistration\Domain\Service\Asset\AssetManipulator;
+use Neos\Demo\UserRegistration\Domain\Service\Resource\ResourceManipulator;
 use Neos\Flow\Core\Bootstrap;
 use Neos\Flow\Package\Package as BasePackage;
+use Neos\Media\Domain\Service\ThumbnailService;
 use Neos\Neos\Controller\Backend\ContentController;
 use Neos\Media\Domain\Service\AssetService;
 
@@ -14,5 +16,6 @@ class Package extends BasePackage
         $dispatcher = $bootstrap->getSignalSlotDispatcher();
         $dispatcher->connect(AssetService::class, 'assetCreated', AssetManipulator::class, 'assetCreated');
         $dispatcher->connect(ContentController::class, 'assetUploaded', AssetManipulator::class, 'assetUploaded');
+        $dispatcher->connect(ThumbnailService::class, 'thumbnailCreated', ResourceManipulator::class, 'thumbnailCreated');
     }
 }
